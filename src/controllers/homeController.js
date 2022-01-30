2dcd3efa4e967e65faf253287f54be262d5435d8
nkzren <renan.nakazawa0@gmail.com>
2022-01-30 17:10:54 -0300

    remove um render nao utilizado

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,14 +6,10 @@ const renderPg1 = (req, res, next) => {
     res.render('pg1');
 }
 
-const renderPg2 = (req, res, next) => {
-    res.render('pg2');
-}
-
 const renderPg3 = (req, res, next) => {
     res.render('pg3');
 }
 
 module.exports = {
-    renderHome, renderPg1, renderPg2, renderPg3,
+    renderHome, renderPg1, renderPg3,
 }
